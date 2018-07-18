044eccc0628b3f693255176ce5465572b11dccd9
Pedro Eusébio <pedroeusebio@poli.ufrj.br>
2018-07-18 02:04:58 -0300

    adding new modifications;

--- correlation.py
+++ correlation.py
@@ -3,6 +3,8 @@ import csv
 import math
 import scipy.stats as stats
 import pandas as pd
+from decimal import *
+import matplotlib.pyplot as plt
 
 
 def read_dataset(dataset_name, number_columns, remove_header=False, delimiter=','):
@@ -30,38 +32,20 @@ def read_dataset(dataset_name, number_columns, remove_header=False, delimiter=',
     else:
         return dataset
 
-def remove_correlation(test_dataset, train_dataset, header, eps=.05):
-    columns_to_remove = []
-    indexes_to_remove = []
-    headers_to_remove = []
+def analising_missing_values(dataset):
+    analise = {}
+    for col in xrange(len(dataset)-1):
+        possible_values = set(dataset[col])
+        analise[col] = {'possible_values': len(possible_values), 'na_percent': Decimal(0)}
+        if 'NA' in list(possible_values):
+            analise[col]['na_percent'] = Decimal(dataset[col].count('NA')) / Decimal(len(dataset[col]))
+    return analise
 
-    for i in xrange(len(test_dataset)):
-        if len(set(train_dataset[i])) == 1:
-            indexes_to_remove.append(i)
-            columns_to_remove.append(train_dataset[i])
-            headers_to_remove.append(header[i])
-            continue
-        if i not in indexes_to_remove:
-            for j in xrange(i, len(test_dataset)):
-                if i != j  and j not in indexes_to_remove:
-                    correlation = stats.pearsonr(train_dataset[i], train_dataset[j])[0]
-                    if abs(correlation) + eps >=  1:
-                        indexes_to_remove.append(j)
-                        columns_to_remove.append(train_dataset[j])
-                        headers_to_remove.append(header[j])
-    for col in columns_to_remove:
-        index = train_dataset.index(col)
-        train_dataset.remove(col)
-        del test_dataset[index]
-
-    for h in headers_to_remove:
-        header.remove(h)
-
-def hashing_trick(train_dataset, test_dataset, unique_values=20):
+def hashing_trick(train_dataset, test_dataset, unique_values=20, cols_to_remove=[]):
     new_header = create_column(train_dataset)
     hashing_trick_columns = []
-    for col in xrange(len(train_dataset) - 1):
-        if len(set(train_dataset[col])) < unique_values:
+    for col in xrange(1,len(train_dataset) - 1):
+        if len(set(train_dataset[col])) < unique_values and col not in cols_to_remove:
             hashing_trick_columns.append(new_header[col])
 
     training_lines = len(train_dataset[0])
@@ -78,14 +62,13 @@ def hashing_trick(train_dataset, test_dataset, unique_values=20):
 
     train_data_frame = pd.concat([data_frame[0:training_lines],
                                   pd.DataFrame(train_dataset[-1], columns=['salesPrice'])], axis=1)
-    print train_data_frame
     test_data_frame = data_frame[training_lines:]
 
     print(len(train_data_frame.columns))
     print(len(test_data_frame.columns))
 
-    train_data_frame.to_csv('dataset/processed/trainWithHashTrick.csv', index = False, header= False)
-    test_data_frame.to_csv('dataset/processed/testWithHashTrick.csv', index = False, header= False)
+    train_data_frame.to_csv('dataset/processed/trainWithHashTrick.csv', index = False, header=False)
+    test_data_frame.to_csv('dataset/processed/testWithHashTrick.csv', index = False, header=False)
 
 def create_column(dataset):
     new_header = []
@@ -93,15 +76,46 @@ def create_column(dataset):
         new_header.append("A" + str(col))
     return new_header
 
-header, train_dataset = read_dataset('dataset/train.csv', 81, remove_header=True)
-test_header, test_dataset = read_dataset('dataset/test.csv', 80, remove_header=True)
+def remove_correlation(corr_df, df, eps=.05, col_to_remove=[]):
+    import pdb; pdb.set_trace()
+    indexes_to_remove = []
+    for i, row in corr_df.iterrows():
+        for j, value in row.iteritems():
+            if abs(value) + eps >= 1 and i != j:
+                indexes_to_remove.append(j)
+    import pdb; pdb.set_trace()
+    return indexes_to_remove
 
+def get_na_cols_to_remove(analise):
+    cols_to_remove = []
+    for col, col_analise in analise.iteritems():
+        if col_analise['na_percent'] >= Decimal(.80):
+            cols_to_remove.append(col)
+    return cols_to_remove
 
-hashing_trick(train_dataset, test_dataset)
+header, train_dataset = read_dataset('dataset/train.csv', 81, remove_header=True, delimiter=',')
+test_header, test_dataset = read_dataset('dataset/test.csv', 80, remove_header=True, delimiter=',')
 
-# header, train_dataset = read_dataset('dataset/train.csv', 582, remove_header=True)
-# test_dataset = read_dataset('dataset/test.csv', 80, remove_header=False)
-# remove_correlation(test_dataset, train_dataset, header=.01)
+analise = analising_missing_values(train_dataset)
+cols_to_remove = get_na_cols_to_remove(analise)
 
+hashing_trick(train_dataset, test_dataset, cols_to_remove=cols_to_remove)
 
+train_df = pd.DataFrame.from_csv('dataset/processed/trainWithHashTrick.csv', index_col=0, header=None)
 
+# remove NA columns
+na_quantity_by_columns = train_df.isnull().sum()
+cols_to_remove = []
+for index, value in na_quantity_by_columns.iteritems():
+    if Decimal(value)/ len(train_df) >= .80:
+        cols_to_remove.append(index)
+for i in cols_to_remove:
+    del train_df[i]
+import pdb; pdb.set_trace()
+filled_train_df = train_df.fillna(train_df.mean())
+corr_df = filled_train_df.corr(method='pearson')
+plt.matshow(corr_df)
+plt.show()
+# indexes_to_remove = remove_correlation(corr_df, train_df, col_to_remove=cols_to_remove)
+
+ 
